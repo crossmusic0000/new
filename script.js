f54c43f126b44a52cfb634e8f259de10b29541ab
crossmusic0000 <76904070+crossmusic0000@users.noreply.github.com>
2021-02-25 21:09:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(function() {
     var $clickable = $('.ripple');
 
-    /* mousedownだと直ぐに発動し、clickだとマウスボタンを離した時に発動する */
     $clickable.on('mousedown', function(e) {
         var _self   = this;
         var x       = e.offsetX;
@@ -11,21 +10,13 @@ $(function() {
         var w       = $effect.width();
         var h       = $effect.height();
 
-        /* クリックした座標を中心とする */
         $effect.css({
             left: x - w / 2,
             top: y - h / 2
         });
 
-        /* jsではclassの付け替えをするだけ */
         if (!$effect.hasClass('is-show')) {
             $effect.addClass('is-show');
-
-            /*
-             * エフェクトアニメーションが終わったらclassを削除する
-             * ここでは、単純にcssで設定するdurationと時間を合わせているだけですが
-             * keyframes終了のイベント(AnimationEnd)が取れるかと思うので、それで対応した方が良いかも
-             */
             setTimeout(function() {
                 $effect.removeClass('is-show');
             }, 750);
@@ -34,3 +25,117 @@ $(function() {
     });
 
 });
+
+document.getElementById("home").style.display = "block";
+document.getElementById("search").style.display = "none";
+document.getElementById("library").style.display = "none";
+document.getElementById("premium").style.display = "none";
+document.getElementById("question").style.display = "block";
+document.getElementById("result").style.display = "none";
+
+function home() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("search").style.display = "none";
+    document.getElementById("library").style.display = "none";
+    document.getElementById("premium").style.display = "none";
+    document.getElementById("question").style.display = "block";
+    document.getElementById("result").style.display = "none";
+}
+function search() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("search").style.display = "block";
+    document.getElementById("library").style.display = "none";
+    document.getElementById("premium").style.display = "none";
+    document.getElementById("question").style.display = "block";
+    document.getElementById("result").style.display = "none";
+}
+function library() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("search").style.display = "none";
+    document.getElementById("library").style.display = "block";
+    document.getElementById("premium").style.display = "none";
+    document.getElementById("question").style.display = "block";
+    document.getElementById("result").style.display = "none";
+}
+function premium() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("search").style.display = "none";
+    document.getElementById("library").style.display = "none";
+    document.getElementById("premium").style.display = "block";
+    document.getElementById("question").style.display = "block";
+    document.getElementById("result").style.display = "none";
+}
+
+
+document.onkeypress = enter;
+function enter(){
+    if( window.event.keyCode == 13 ){
+        var target = document.getElementById("search-btn");
+        var word = target.value;
+        searchWord({term: word,limit: 20});
+    }
+}
+
+
+var searchWord = function getInfo(options) {
+    var params = {
+        lang: 'ja_jp',
+        entry: 'music',
+        media: 'music',
+        country: 'JP',
+    };
+        
+    if (options && options.term) {
+        params.term = options.term;
+    }
+        
+    if (options && options.limit) {
+        params.limit = options.limit;
+    }
+          
+    $.ajax({
+        url: 'https://itunes.apple.com/search',
+        method: 'GET',
+        data: params,
+        dataType: 'jsonp',
+            
+        success: function(json) {
+            showData(json);
+        },
+
+        error:function () {
+            document.getElementById("home").style.display = "none";
+            document.getElementById("search").style.display = "block";
+            document.getElementById("library").style.display = "none";
+            document.getElementById("premium").style.display = "none";
+            document.getElementById("question").style.display = "none";
+            document.getElementById("result").style.display = "block";
+            document.getElementById("result").innerHTML = '<p style="margin-left: 10px;margin-top: 20px;">インターネット接続がありません。</p>';
+        }
+    });
+};
+        
+function showData(json) {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("search").style.display = "block";
+    document.getElementById("library").style.display = "none";
+    document.getElementById("premium").style.display = "none";
+    document.getElementById("question").style.display = "none";
+    document.getElementById("result").style.display = "block";
+    if (json.results.length != 0) {//length
+        var result = document.getElementById("result");
+        html  = '<h2 style="margin-left: 10px;margin-top: 20px;">検索結果</h2>';
+                
+        for (var i = 0, len = json.results.length; i < len; i++) {//length
+            var resultWord = json.results[i];
+        
+            html += '<div style="width: 96%;height: 110px;margin-left: 2%;margin-top: 10px;background: #419be0;">';
+            html += '<a href="' + resultWord.trackViewUrl + '&amp;at=アフィリエイト・トークン" rel="nofollow" target="_blank">';
+            html += '<img style="width: 110px;height: 110px;" src="' + resultWord.artworkUrl100 + '" class="itunes-embed-image"/></a><br>';
+            html += '<h4 class="itunes-embed-artist">' + resultWord.trackName + '</h4>';
+            html += '<h4 style="color: rgb(105, 105, 105);">' + resultWord.artistName + '</h4></div>';
+            //html += '<button id="' + resultWord.trackId + '" style="width: 20%;height: 40px;font-size: 20px;color: white;background: rgb(32, 147, 201);outline: none;border: none;border-radius: 5px;cursor: pointer;">視聴する</button></div><br>'
+        }
+    }
+    result.innerHTML = html;
+}
